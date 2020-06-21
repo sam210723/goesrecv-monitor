50a0e97585d941583e407afc26b97b2b28706d55
Sam <sam210723@hotmail.com>
2020-06-21 23:19:31 +1000

    Remove unused imports

--- goesrecv-monitor/Stats.cs
+++ goesrecv-monitor/Stats.cs
@@ -1,12 +1,9 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace goesrecv_monitor
 {
--- goesrecv-monitor/Symbols.cs
+++ goesrecv-monitor/Symbols.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
-using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace goesrecv_monitor
 {
